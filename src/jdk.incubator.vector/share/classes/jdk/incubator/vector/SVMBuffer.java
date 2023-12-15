db82ca06fed4bc98a75957176f2101b50dbd669b
Reto Ehrensperger <ehrensperger.reto@gmail.com>
2023-12-15 17:49:58 +0100

    fix mulRange error

--- src/jdk.incubator.vector/share/classes/jdk/incubator/vector/SVMBuffer.java
+++ src/jdk.incubator.vector/share/classes/jdk/incubator/vector/SVMBuffer.java
@@ -175,7 +175,7 @@ public class SVMBuffer {
      *  @param amount of elements to be multiplied
      *  @return the new SVMBuffer of size amount containing the multiplied elements
      */
-    public SVMBuffer mulRange(int index1, SVMBuffer factor, int index2, int amount,){
+    public SVMBuffer mulRange(int index1, SVMBuffer factor, int index2, int amount){
         SVMBuffer results = new SVMBuffer(info, amount);
         SVMBufferSupport.MultiplyRange(info.GetProgram(), info.GetCommandQueue(), this.svmBuffer, index1, factor.svmBuffer, index2, results.svmBuffer, amount);
         return results;
